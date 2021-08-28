d804cc98d2a3590a68da86a0993da1251e76be98
Stepfen Shawn <m18824909883@163.com>
2021-08-28 05:55:05 -0500

    Add files via upload

--- extension.ts
+++ extension.ts
@@ -14,15 +14,6 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        //只看这个地方'extension.sayHello'和 package.json 里面的 "onCommand:extension.sayHello" 是一个对应关系
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
     }
 
     // this method is called when your extension is deactivated
