0c597b97e4751270a30d6221498330f20d417f41
StepfenShawn <stepfencurryxiao@gmail.com>
2022-08-21 23:57:03 +0800

    add todo-list

--- README.md
+++ README.md
@@ -5,6 +5,9 @@
 * 支持语法高亮
 * 代码自动补全
 
+# TODO
+* 使用快捷键运行Cantonese代码
+
 ## 使用方法
 
 git clone最新版repo后直接F5（或在debug面板中点击启动）启动插件。
--- extension.ts
+++ extension.ts
@@ -14,7 +14,14 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    }
+    
+    let disposable = vscode.command.registerCommand("extension.PrintCantoneseVersion",
+        () => {
+            vscode.windows.showInformationMessage("Hello World!")
+        });
+
+    context.subscriptions.push(disposable);
+}
 
     // this method is called when your extension is deactivated
     export function deactivate() {
